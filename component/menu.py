c41a3726f7f61834f835d54f62d0f9f3fb70d29f
kkdhandhal13857 <krushna.dhandhal@gmail.com>
2021-07-13 18:42:44 +0530

    Commit 3 with images folder

--- component/menu.py
+++ component/menu.py
@@ -1,22 +1,20 @@
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.list import MDList
 from kivymd.uix.label import MDLabel
-from kivymd.uix.list import TwoLineAvatarListItem
+from kivymd.uix.list import TwoLineAvatarListItem, ImageLeftWidget
 from kivymd.uix.button import MDIconButton
 
 class UserDetail(MDBoxLayout):
     def __init__(self, **kwargs):
         super().__init__()
         self.orientation = "horizontal"
-        self.size_hint_y = 0.1
-
         self.ToolIcon = MDIconButton()
         self.ToolIcon.icon = "images/user.png"
         self.ToolIcon.pos_hint = {"center_x": .05, "center_y": .5}
-        self.ToolIcon.user_font_size = "64sp"
+        self.ToolIcon.user_font_size = 40
         self.ToolIcon.radius = [36, 36, 0, 0, ]
-
         self.UsrAreaDescrLayout = MDBoxLayout()
+
         self.UsrAreaDescrLayout.orientation = "vertical"
         self.UsrAreaDescrLayout.add_widget(MDLabel(text="User1", halign="left"))
         self.UsrAreaDescrLayout.add_widget(MDLabel(text="xxxxxx-x-x-x-xxx-x", halign="left"))
@@ -26,15 +24,20 @@ class UserDetail(MDBoxLayout):
         self.add_widget(self.UsrAreaDescrLayout)
 
 class MenuList(MDList):
-    def __init__(self,**kwargs):
-        super.__init__()
+    def __init__(self, **kwargs):
+        super().__init__()
         self.home_link = TwoLineAvatarListItem()
+        self.home_link.text="Dashbord"
+        self.home_link.secondary_text="your home page"
+        self.home_link.add_widget(ImageLeftWidget(source="images/home.png"))
+        self.add_widget(self.home_link)
 
 class NavMenu(MDBoxLayout):
     def __init__(self,**kwargs):
         super().__init__()
         self.orientation = "vertical"
+
         self.padding = "8dp"
         self.spacing ="8dp"
         self.add_widget(UserDetail())
-        self.add_widget(MDLabel(text="Menu list", halign="center"))
+        self.add_widget(MenuList())
